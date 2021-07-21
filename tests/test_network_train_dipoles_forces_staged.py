fa12118c76992dee37a35b1b95f13397e78e16a4
Forgotten <lzepeda@math.mit.edu>
2021-07-21 13:12:49 -0500

    tests: modified the evolution of the weights

--- tests/test_network_train_dipoles_forces_staged.py
+++ tests/test_network_train_dipoles_forces_staged.py
@@ -1,4 +1,8 @@
 # script to check that the network works as intended
+# we train a network using dipole type of data 
+# using a multi-stage optimization loop with different batchsizes
+
+
 import numpy as np 
 import matplotlib.pyplot as plt
 import torch
@@ -15,175 +19,96 @@ from utilities_3d import smooth_cut_off
 from utilities_3d import DenseChainNet
 from networks_3d import DescriptorModule
 from networks_3d import DeepMDsimpleEnergyForces
-from data_gen_3d import gen_data_per_3d_mixed
+from data_gen_3d import gen_data_exp_per_3d_charge
 
-
-import os
-
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
-nameScript = sys.argv[0].split('/')[-1]
-
-# we are going to give all the arguments using a Json file
-# nameJson = sys.argv[1]
-print("=================================================")
-# print("Executing " + nameScript + " following " + nameJson, flush = True)
-print("Executing " + nameScript, flush = True)
-print("=================================================")
-
-plot_bool = False
-normalization_bool = False
-
-np.random.seed(1234)
-# we check that the construction of the moduledict is 
-# properly performed :)
-
-## we create the testing example
-n_val = 100
+ngpu = 1 
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu") 
+print(device)
 
 # number of particles
-length_cell = 1.0
 n_cells = 3
-n_points_cell = 2
+n_points_cell = 1
+n_val = 100
 n_snaps = 3200 + n_val
-n_points = n_cells**3
-L = n_cells*length_cell
+n_points = n_cells**3*(2*n_points_cell)
+L = 3.
 
-min_delta = 0.3
-radious = L/2
+min_delta = 0.5
 
+mu = 10.0
+# structured 
+r_in,\
+charge_array,\
+potential_array,\
+forces_array = gen_data_exp_per_3d_charge(n_cells, n_points_cell, 
+                                          n_snaps, L/n_cells, 
+                                          min_delta, mu = mu)
 
-mu1 = 10.0
-mu2 = 5.0
-
-alpha1 = 1.0
-alpha2 = 0.0
-
-pointsArray,\
-potentialArray,\
-forcesArray = gen_data_per_3d_mixed(n_cells, n_points_cell, 
-                                    mu1, mu2, 
-                                    n_snaps, min_delta, 
-                                    L/n_cells, 
-                                    alpha1, alpha2)
-
-print("Parameters")
-print("mu1 = %.1f\t mu2 = %.1f"% (mu1, mu2))
-print("alpha1 = %.1f\t alpha2 = %.1f"% (alpha1, alpha2))
-print("min delta = %.1f\t L = %.1f"%(min_delta, L))
+plot_bool = False 
 
 if plot_bool:
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(r_in[0,:,0], r_in[0,:,1], r_in[0,:,2], marker='.',  s=10)
+    ax.scatter(r_in[1,:,0], r_in[1,:,1], r_in[1,:,2], marker='.',  s=10)
+    plt.show()
+
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
     idx_plot = 10
-    ax.scatter(pointsArray[idx_plot,:,0],
-               pointsArray[idx_plot,:,1], 
-               pointsArray[idx_plot,:,2], marker='.',  s=5)
+    ax.scatter(r_in[idx_plot,:,0],
+               r_in[idx_plot,:,1], 
+               r_in[idx_plot,:,2], marker='.',  s=5)
 
-    ax.quiver(pointsArray[idx_plot,:,0],
-              pointsArray[idx_plot,:,1], 
-              pointsArray[idx_plot,:,2], 
-              forcesArray[idx_plot,:,0],
-              forcesArray[idx_plot,:,1], 
-              forcesArray[idx_plot,:,2], length=1, color = 'black')
+    ax.quiver(r_in[idx_plot,:,0],
+              r_in[idx_plot,:,1], 
+              r_in[idx_plot,:,2], 
+              forces_array[idx_plot,:,0],
+              forces_array[idx_plot,:,1], 
+              forces_array[idx_plot,:,2], length=1, color = 'black')
 
 
     plt.show()
 
-mean_pot, std_pot = potentialArray.mean(), potentialArray.std()
+
+# creating the atom type array
+atom_type = np.array(charge_array == 1.,\
+                     dtype=np.int64).reshape((n_snaps, n_points))
+
+#creating the data
+L_torch = torch.tensor(L)
+r_in_torch = torch.tensor(r_in, dtype=torch.float64)
+r_in_torch.requires_grad = True 
+atom_type_torch = torch.tensor(atom_type)
 
 
-print("Potential mean {:.6f}\t std {:.6f}".format(mean_pot, std_pot))
+##################################################################
+####   Setting up the training data   ############################
+##################################################################
 
-if normalization_bool:
-    print("Normalizing the potential")
-    potentialArray = (potentialArray - mean_pot)/std_pot
-    forcesArray = forcesArray/std_pot
+radious = L/2
 
-# backwards compatibility with only one species of atom
-r_in_torch = torch.tensor(pointsArray, dtype=torch.float32)
-input_types_torch = torch.tensor(np.zeros(pointsArray.shape[:-1]), dtype=torch.int64)
+max_num_neighs_type = np.array([n_points//2, n_points//2], dtype=np.int64)
+
+r_in_torch = torch.tensor(r_in, dtype=torch.float32)
+input_types_torch = torch.tensor(atom_type, 
+                                 dtype=torch.int64)
 
 # define the data (we need to detach it from the computation)
-energy_train = torch.tensor(potentialArray.reshape((-1,)), dtype = torch.float32)  
-forces_train = torch.tensor(forcesArray, dtype = torch.float32)  
-
-L_torch = torch.tensor(L, dtype = torch.float32)
-
-descript_dim = [1, 2, 4, 8, 16, 32]
-fitting_dim = [64, 64, 64, 64]
-
-max_num_neighs_type = np.array([54])
-# only one type
-a_types = [0]
-
-av = torch.tensor([0.0, 0.0], dtype = torch.float32)
-std = torch.tensor([1.0, 1.0], dtype = torch.float32)
-
-inter_list = comput_inter_list_type(pointsArray, 
-                                    input_types_torch.numpy(), 
-                                    L, radious, 
-                                    max_num_neighs_type)
-
-inter_list_torch = torch.tensor(inter_list, dtype= torch.int64)
-
-r_c = radious
-r_cs = 4/5*radious
-
-DeepMD = DeepMDsimpleEnergyForces(n_points, # 
-               L_torch, # we need it to be a tensor
-               a_types, 
-               max_num_neighs_type, # this needs to be a tensor too
-               descript_dim,
-               fitting_dim,
-               False,
-               [r_cs, r_c],
-               4,
-               torch.nn.GELU(), # torch.selu/ nn.SiLU()/ torch.tanh / torch.relu / torch.nn.GELU()
-               True, 
-               False, # batch normalization at each layer
-               True, # smooth-cut off
-               av,
-               std)  
-                       
-
-# DeepMD = torch.jit.script(DeepMDsimpleEnergyForces(n_points, # 
-#                L_torch, # we need it to be a tensor
-#                a_types, 
-#                max_num_neighs_type, # this needs to be a tensor too
-#                descript_dim,
-#                fitting_dim,
-#                False,
-#                [r_cs, r_c],
-#                8,
-#                F.relu)).to(device)
-
-# moving the model to a GPU
-
-print("Checking if GPU is present")
-ngpu = 0
-
-if (torch.cuda.is_available() and ngpu > 0):
-    device = torch.device("cuda:0")  
-else:
-    device = torch.device("cpu") 
+energy_train = torch.tensor(potential_array.reshape((-1,)), 
+                            dtype = torch.float32)  
+forces_train = torch.tensor(forces_array, 
+                            dtype = torch.float32)  
 
 
-print("Moving the model to %s"%device)
-DeepMD = DeepMD.to(device)
+###### Validation data 
 
+inter_list_val_np = comput_inter_list_type(r_in[-n_val:,:,:], atom_type[-n_val:,:], 
+                                    L, radious, max_num_neighs_type)
 
-print("Testing that the model was moved properly")
-r_in_cuda,\
-input_types_cuda,\
-inter_list_cuda= r_in_torch[:2,:,:].to(device), \
-                  input_types_torch[:2,:].to(device), \
-                  inter_list_torch[:2,:,:].to(device)
-
-energyNN, forcesNN = DeepMD(r_in_cuda, 
-                            input_types_cuda, 
-                            inter_list_cuda)
+inter_list_torch = torch.tensor(inter_list_val_np, dtype=torch.int64)
 
 
 r_in_val,\
@@ -195,6 +120,53 @@ inter_list_val= r_in_torch[-n_val:,:,:].to(device), \
 energy_val = energy_train[-n_val:].to(device)
 forces_val = forces_train [-n_val:,:,:].to(device)
 
+
+####### Parameters of the Model
+
+descript_dim = [1, 2, 4, 8, 16, 32]
+fitting_dim = [64, 64, 64, 64]
+a_types = [0, 1]
+
+av = torch.tensor([0.0, 0.0], dtype = torch.float32)
+std = torch.tensor([1.0, 1.0], dtype = torch.float32)
+
+r_c = radious
+r_cs = 4/5*(r_c)
+
+DeepMD = DeepMDsimpleEnergyForces(n_points, # 
+               L_torch, # we need it to be a tensor
+               a_types, 
+               max_num_neighs_type, # this needs to be a tensor too
+               descript_dim,
+               fitting_dim,
+               False,
+               [r_cs, r_c],
+               2,
+               torch.nn.GELU(), # torch.selu/ nn.SiLU()/ torch.tanh / torch.relu / torch.nn.GELU()
+               True, 
+               False, # batch normalization at each layer
+               True, # smooth-cut off
+               av,
+               std)  
+
+# model = torch.jit.script(DeepMDsimpleEnergyForces(n_points, # 
+#                L_torch, # we need it to be a tensor
+#                a_types, 
+#                max_num_neighs_type, # this needs to be a tensor too
+#                descript_dim,
+#                fitting_dim,
+#                False,
+#                [r_cs, r_c],
+#                8,
+#                F.relu)).to(device)
+
+# energyNN, forcesNN = DeepMD(r_in_torch, input_types, inter_list_torch)
+# yey! at lease this produces something! 
+
+### Setting up the optimization parameters
+
+DeepMD = DeepMD.to(device)
+
 ### setting up the optimization parameters
 print("Setting up the optimization parameters")
 
@@ -203,16 +175,15 @@ criterion = torch.nn.MSELoss(reduction='mean')
 
 # specifying the learning rate
 learningRate =1.e-3
-lr_steps = 4
-gamma = 0.99
+lr_steps = 5
+gamma = 0.98
 
 # specify optimizer
 optimizer = torch.optim.Adam(DeepMD.parameters(), lr=learningRate)
 
 # specidy scheduler
 exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                             step_size=lr_steps,
-                                             gamma=gamma)
+                                             step_size=lr_steps, gamma=gamma)
 
 
 # creating the data sets (we don't consider testing so far)
@@ -222,13 +193,8 @@ datasetTrain = torch.utils.data.TensorDataset(r_in_torch,
                                               forces_train) 
 
 
-
-batch_sizes_array = [16]
-n_epochs_array = [800]
-
-# cumulative number of epochs 
-n_epochs_array_cum = np.zeros((len(n_epochs_array)+1))
-n_epochs_array_cum[1:] = np.cumsum(np.array(n_epochs_array))
+batch_sizes_array = [2, 4, 8, 16, 32, 64, 128]
+n_epochs_array = [12, 25, 50, 100, 200, 400, 800]
 
 
 for ii, (batch_size, n_epochs) in enumerate(zip(batch_sizes_array,\
@@ -243,11 +209,11 @@ for ii, (batch_size, n_epochs) in enumerate(zip(batch_sizes_array,\
                                                   num_workers=4)
 
     ## these parameters need to be adjusted 
-    weightE_init = 0.02
-    weightE_final = 8.0
+    weightE_init = 0.
+    weightE_final = 0.0
      
-    weightF_init = 100.0
-    weightF_final = 1.
+    weightF_init = 1.0
+    weightF_final = 1.0
 
     print("Starting the training loop")
     for epoch in range(1, n_epochs+1):
@@ -258,17 +224,15 @@ for ii, (batch_size, n_epochs) in enumerate(zip(batch_sizes_array,\
         start = time.time()
 
         # this shouldn't be linear, it should be exponential
-        # weights_rate = (epoch+n_epochs_array_cum[ii])/n_epochs_array_cum[-1]
-        # weights_rate = epoch/n_epochs
-        # weightE = weightE_init*(1-weights_rate) + weightE_final*weights_rate
-        # weightF = weightF_init*(1-weights_rate) + weightF_final*weights_rate
+        weights_rate = epoch/n_epochs
+        weightE = weightE_init*(1-weights_rate) + weightE_final*weights_rate
+        weightF = weightF_init*(1-weights_rate) + weightF_final*weights_rate
 
+        # rate_E = np.power(weightE_final/weightF_init, 1/n_epochs)
+        # rate_F = np.power(weightF_final/weightF_init, 1/n_epochs) 
 
-        rate_E = np.power(weightE_final/weightF_init, 1/n_epochs)
-        rate_F = np.power(weightF_final/weightF_init, 1/n_epochs) 
-
-        weightE = weightE_init*np.power(rate_E,epoch)
-        weightF = weightF_init*np.power(rate_F,epoch)
+        # weightE = weightE_init*np.power(rate_E,epoch)
+        # weightF = weightF_init*np.power(rate_F,epoch)
 
         ###################
         # train the model #
@@ -294,7 +258,7 @@ for ii, (batch_size, n_epochs) in enumerate(zip(batch_sizes_array,\
             # calculate the loss for the enery
             lossF = criterion(forcesNN, forces_batch)
             # adding the losses together
-            loss =   weightF*lossF + weightE*lossE
+            loss =   weightF*lossF  + weightE*lossE
             # backward pass: compute gradient of the loss with respect to model parameters
             loss.backward()
             # perform a single optimization step (parameter update)
@@ -340,3 +304,6 @@ for ii, (batch_size, n_epochs) in enumerate(zip(batch_sizes_array,\
             print('Weights Energy: {:.3e}\t Forces: {:.3e} [s]'.format(
             weightE,
             weightF), flush=True)
+
+
+torch.save(DeepMD, 'model_deep_md_forces_dipole.pt')
